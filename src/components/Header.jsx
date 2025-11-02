3b6efc1701a4b96d26bdf11ef37339d87fd34caf
RohitSingh-dev <rohitsingh20082002@gmail.com>
2025-11-03 01:01:58 +0530

    updated CV and name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
       <div className="header">
-          <span style={{marginLeft: "-230px"}}>Rohit .dev</span>
+          <span style={{marginLeft: "-230px"}}>Rohit-dev</span>
           <span style={{paddingLeft: "200px"}}>We are adding and updating things for you. Stay tuned.</span>
       </div>
   )
